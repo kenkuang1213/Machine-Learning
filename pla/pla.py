3bc4434f249da17ba6c14aec19b1bf999986a32a
kenkuang <kenkuang1213@gmail.com>
2015-01-14 04:50:57 +0800

    fix pla.py

--- pla/pla.py
+++ pla/pla.py
@@ -1,57 +1,50 @@
 import sys
 import random
 import re
-            
-data = [re.split('\s', line) for line in open('data.dat', 'r')]
-w = [0.0, 0.0, 0.0,0.0]
+import numpy
+
+datas = [re.split('\s|\t1', line) for line in open('data.dat', 'r')]
+for i in range(0, len(datas)):
+    datas[i] = datas[i][0:5]
+    for j in range(0, 4):
+        datas[i][j] = float(datas[i][j])
+    datas[i][4] = int(datas[i][4])
+
+w = [0.0, 0.0, 0.0, 0.0, 0.0]
 
 times = 0
-	
-change = True;
-while change:	
-            change=False
-	for i in range(0, len(data)):
-	            x = [float(t) for t in data[i][0:4]]
-	            y=int(data[i][4])
-	            ans = 0
-	            for j in range(0, len(x)):
-	                        ans += w[j] * x[j]	      
-	            if (ans > 0 and y < 0) or (ans < 0 and y> 0) or (ans== 0  and y > 0):
-			print(w)
-			print(x)
-			times += 1
-			change =True
-			w = [w[k] + x[k] * float(data[i][4])	     for k in range(0, len(x))]
-      change=False
-
-      	             	
+change = True
+while change:
+    change = False
+    for data in datas:
+        x = data[0:4]
+        x.insert(0, 1)
+        y = data[4]
+        if (numpy.dot(x, w) > 0 and y < 0) or (numpy.dot(x, w) <= 0 and y > 0):
+            times += 1
+            change = True
+            wt = [w[k] + (x[k] * y) for k in range(0, len(x))]
+            w = wt
+    # change = False
 print("naive cycle update " + str(times) + " times")
-print(w)
-avgtimes = 0
-for num in range(0, 2000):
-            start = random.randint(0, len(data) - 1)
-            w = [float(i) for i in data[start][0:4]]
-            tmpTimes = 0
+avgTimes = 0
+for i in range(0, 2000):
+    tmptimes = 0
+    change = True
+    ranDatas = datas
+    random.shuffle(ranDatas)
+    w = [0.0, 0.0, 0.0, 0.0, 0.0]
+    while change:
+        change = False
+        for data in ranDatas:
+            x = data[0:4]
+            x.insert(0, 1)
+            y = data[4]
+            if (numpy.dot(x, w) > 0 and y < 0) or (numpy.dot(x, w) <= 0 and y > 0):
+                tmptimes += 1
+                change = True
+                wt = [w[k] + (x[k] * y) for k in range(0, len(x))]
 
-            for i in range(start, len(data)):
-                        x = [float(t) for t in data[i][0:4]]
-                        ans = 0
-                        for j in range(0, len(x)):
-                                    ans += float(w[j]) * float(x[j])
-                        if (ans > 0 and (int(data[i][4])) < 0) or (ans < 0 and (int(data[i][4])) > 0):
-                                    tmpTimes += 1
-                        w = [w[k] + x[k] * float(data[i][4])
-                             for k in range(0, len(x))]
-            for i in range(0, start):
-                        x = [float(t) for t in data[i][0:4]]
-                        ans = 0
-                        for j in range(0, len(x)):
-                                    ans += float(w[j]) * float(x[j])
-                        if (ans > 0 and (int(data[i][4])) < 0) or (ans < 0 and (int(data[i][4])) > 0):
-                                    tmpTimes += 1
-                                    w = [w[k] + x[k] * float(data[i][4])
-                                         for k in range(0, len(x))]
-
-            avgtimes += tmpTimes
-print("random cycle update " + str(avgtimes / 2000) + " times")
-	
+                w = wt
+    avgTimes += tmptimes
+print("random cycle update " + str(avgTimes / 2000) + " times")
