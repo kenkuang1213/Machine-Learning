ea497683b8a80950230c1c23b1d1909a4abdf6cc
kenkuang <kenkuang1213@gmail.com>
2015-02-02 01:53:40 +0800

    fix bugs

--- pocket/pocket.py
+++ pocket/pocket.py
@@ -13,9 +13,9 @@ def transDataToInt(datas):
     return datas
 
 
-def checkErrors(datas, w):
+def checkErrors(Datas, w):
     errors = 0
-    for data in datas:
+    for data in Datas:
         x = data[0:4]
         y = data[4]
         x.insert(0, 1)
@@ -24,35 +24,37 @@ def checkErrors(datas, w):
     return errors
 
 
-datas = [re.split('\s|\t1', line) for line in open('data.dat', 'r')]
+testNum = 20
+
+datas = [re.split('\s|\t1', line) for line in open('hw1_18_train.dat', 'r')]
 datas = transDataToInt(datas)
-testDatas = [re.split('\s|\t1', line) for line in open('data_test.dat', 'r')]
+testDatas = [re.split('\s|\t1', line) for line in open('hw1_18_test.dat', 'r')]
 testDatas = transDataToInt(testDatas)
-w = [0.0, 0.0, 0.0, 0.0, 0.0]
 
-TotalErrorRate = 0
 
-for i in range(0, 200):
+TotalErrorRate = 0.0
+
+for i in range(0, testNum):
     change = True
     ranDatas = datas
     random.shuffle(ranDatas)
     w = [0.0, 0.0, 0.0, 0.0, 0.0]
+    pockW = w
     updates = 0
-    wErrors = sys.maxint
-    while updates <= 50:
+    wErrors = checkErrors(datas, w)
+    while updates < 50:
         for data in ranDatas:
             x = data[0:4]
             x.insert(0, 1)
             y = data[4]
             if (numpy.dot(x, w) > 0 and y < 0) or (numpy.dot(x, w) <= 0 and y > 0):
                 updates += 1
-                if(updates > 49):
-                    break
-                wt = [w[k] + (x[k] * y) for k in range(0, len(x))]
-                temp = checkErrors(testDatas, wt)
+                w = [w[k] + (x[k] * y) for k in range(0, len(x))]
+                temp = checkErrors(ranDatas, w)
                 if(temp < wErrors):
-                    w = wt
+                    pockW = w
                     wErrors = temp
-
-    TotalErrorRate += float(wErrors) / len(testDatas)
-print("avg Error Rate " + str(TotalErrorRate / 2000))
+    TotalErrorRate += float(checkErrors(testDatas, w)
+                            ) / float(len(testDatas))
+    # TotalErrorRate += float(wErrors) / len(testDatas)
+print("avg Error Rate " + str(TotalErrorRate / testNum))
